b809560ac8a5a156b9cdee06b3e8ccfc980b72ea
Diego Ruiz <diegoruiztrabanino13@gmail.com>
2021-02-11 09:41:43 -0600

    Clase jugadores

---
+++ Laboratorio1/Models/Jugador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio1.Models
+{
+    public class Jugador
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public double Salary { get; set; }
+        public string Position { get; set; }
+        public string Club { get; set; }
+
+    }
+}
